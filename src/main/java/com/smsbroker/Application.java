d5adc3609ce5a22a33faed2d395cdcfecaa87727
Raphael Rodrigues da Costa <rxonda@gmail.com>
2016-07-19 01:07:35 -0300

    Some cleanup

--- src/main/java/com/smsbroker/Application.java
+++ src/main/java/com/smsbroker/Application.java
@@ -4,17 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
 import org.springframework.web.client.RestTemplate;
 
-import javax.jms.ConnectionFactory;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @SpringBootApplication
-@EnableJms
 public class Application {
 
     @Bean
@@ -25,13 +20,6 @@ public class Application {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean // Strictly speaking this bean is not necessary as boot creates a default
-    JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory) {
-        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        return factory;
     }
 
     public static void main(String[] args) {
